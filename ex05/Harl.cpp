5bf51b93e7b511b6f7fd9295a534489002fb9418
한예슬 <letmeshare46@gmail.com>
2022-10-15 18:02:02 +0900

    turn member function to static. since they don't use variable of instance.

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -7,33 +7,27 @@ void Harl::complain(std::string level) {
 		std::string("WARNING"), \
 		std::string("ERROR")};
 
-	/* function pointer looks like: int	(*f)(int, int)*/
-	void (Harl::*complainsPTR[4])(void) = \
-		{&Harl::debug, \
-		&Harl::info, \
-		&Harl::warning, \
-		&Harl::error};//레퍼런스로 저장. 객체가 없어도 접근 가능하다.
+	/* basic function pointer looks like: int (*f)(int, int)*/
+	void (*complainsPTR[4])(void) = {debug, info, warning, error};
 
     for (int i = 0; i < 4; i++)
         if (level == levels[i])
-			(this->*complainsPTR[i])();
+			(*complainsPTR[i])();
 
 }
 
 void Harl::debug(void) {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "you need to debug here: blabla~~" << std::endl;
 }
 
 void Harl::info(void) {
-	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
-	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "maybe you need this information: blabla~~" << std::endl;
 }
 
 void Harl::warning(void) {
-	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
-	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "handle or ignore this: blabla~~" << std::endl;
 }
 
 void Harl::error(void) {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "need manual intervention here: blabla~~" << std::endl;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -16,12 +16,17 @@ int main(void) {
 	std::cout << "\n* Harl complains to inform you of the error. *" << std::endl;
 	harl.complain("ERROR");
 
-	std::cout << "\n* Harl complains to warn you, again. *" << std::endl;
-	harl.complain("WARNING");
+	Harl harl2;
 
-	std::cout << "\n* Harl complains for your debugging, again. *" << std::endl;
-	harl.complain("DEBUG");
+	std::cout << "\n* Harl2 complains to warn you, again. *" << std::endl;
+	harl2.complain("WARNING");
 
-	std::cout << "\n* Harl complains to give you information, again. *" << std::endl;
-	harl.complain("INFO");
+	std::cout << "\n* Harl2 complains to inform you of the error. *" << std::endl;
+	harl2.complain("ERROR");
+
+	std::cout << "\n* Harl2 complains for your debugging, again. *" << std::endl;
+	harl2.complain("DEBUG");
+
+	std::cout << "\n* Harl2 complains to give you information, again. *" << std::endl;
+	harl2.complain("INFO");
 }
