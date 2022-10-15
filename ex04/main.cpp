53c10e830224aaf4f6ebce2358b586b63e15f64e
한예슬 <letmeshare46@gmail.com>
2022-10-15 15:07:26 +0900

    ex04: add test files, add comments

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,62 +2,57 @@
 #include <fstream>
 
 int main(int argc, char **argv) {
-	if (argc != 4) {
-		std::cout << "Error: 4 arguments are needed" << std::endl;
-		return 1;
-	}
-	
-	std::string filename = argv[1];
-	std::ifstream in(filename);
-	if (in) {
-		////go to the end of the file
-		in.seekg(0, std::ios::end);
+	if (argc == 4) {
+		/* 1. read the whole content from the file to the string
+			by using the position indicator(pos.) */
+		std::string filename = argv[1];
+		std::ifstream in(filename);
+		if (in) {
+			in.seekg(0, std::ios::end);//move pos. to the end of the file
+			int filesize = in.tellg();//index of the pos.
+			std::string buf;
+			buf.resize(filesize);//resize the buf size same as the file size
+			in.seekg(0, std::ios::beg);//move pos. to the begin of the file
+			in.read(&buf[0], filesize);
 
-		////get filesize
-		int filesize = in.tellg();
+			/* 2. change every s1 to s2 in the string */
+			if (!buf.empty()) {
+				std::string s1 = argv[2];
+				std::string s2 = argv[3];
 
-		////resize buf
-		std::string buf;
-		buf.resize(filesize);
+				std::size_t pos = 0;
+				while (true) {
+					pos = buf.find(s1, pos);//try to find s1
+					if (pos == std::string::npos)//(find function returns npos if it fail)
+						break ;
+					buf = buf.substr(0, pos) + s2 + buf.substr(pos + s1.length());
+					pos = pos + s2.length();//pass some character to delete s2
+				}
 
-		////go to the begin of the file
-		in.seekg(0, std::ios::beg);
+				/* 3. put the changed content from the string to the new file */
+				std::ofstream out(filename + ".replace");
+				if (out) {
+					out << buf;
+				}
 
-		////keep all file contents in string buf
-		in.read(&buf[0], filesize);
-
-		in.close();
-
-		if (buf.empty()) {
-			std::cout << "Error: empty file" << std::endl;
-			return 1;
-		}
-
-		//2. change s1 to s2
-		std::string s1 = argv[2];
-		std::string s2 = argv[3];
-
-		std::size_t pos = 0;
-		while (true) {
-			pos = buf.find(s1, pos);
-			if (pos == std::string::npos)
-				break ;
-			buf = buf.substr(0, pos) + s2 + buf.substr(pos + s1.length());
-			pos = pos + s1.length();
-		}
-
-		//3. put buf to out
-		std::ofstream out(filename + ".replace");
-		if (out) {
-			out << buf;
+	/* 0. Error handling */
+				else {
+					std::cout << "Error: ofstream creation failed" << std::endl;
+					return 1;
+				}
+			}
+			else {
+				std::cout << "Error: empty file" << std::endl;
+				return 1;
+			}
 		}
 		else {
-			std::cout << "Error: ofstream creation failed" << std::endl;
+			std::cout << "Error: ifstream creation failed(no such file or no authorization or extra)" << std::endl;
 			return 1;
 		}
 	}
 	else {
-		std::cout << "Error: ifstream creation failed" << std::endl;
+		std::cout << "Error: 4 arguments are needed" << std::endl;
 		return 1;
 	}
 	return 0;
